5c76501eee8d673a0071a977378378e7b67fe9d5
alhammadi7 <m_alhammadi@ymail.com>
2026-04-25 07:49:56 +0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
             },
             gemini: {
                 key: process.env.GEMINI_API_KEY,
-                url: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
+                url: 'https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent',
                 model: 'gemini-1.5-flash'
             }
         };
